f6539385687241d6026da6303eca1949b8df6570
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-09-15 13:08:57 +0530

    only @cuchd.in domain regex code updated (in progress)

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,25 +1,28 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 export const RegisterSchema = z.object({
-    email: z.string().email({
-        message: " Please enter a valid email address."
-    }),
-    name: z.string().min(1, {
-        message: "Name is required."
-    }),
-    password: z.string().min(6, {
-        message: "Password must be at least 6 characters long."
-    }),
-    passwordConfirmation: z.string().min(6, {
-        message: "Password must be at least 6 characters long."
-    })
-})
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  // .refine((email) => email.endsWith("@cuchd.in"), {
+  //   message: "Only @cuchd.in email addresses are allowed.",
+  // }),
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long.",
+  }),
+  passwordConfirmation: z.string().min(6, {
+    message: "Password must be at least 6 characters long.",
+  }),
+});
 
 export const LoginSchema = z.object({
-    email: z.string().email({
-      message: "Please enter a valid email address",
-    }),
-    password: z.string().min(1, {
-      message: "Please enter a valid password",
-    }),
-  });
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(1, {
+    message: "Please enter a valid password",
+  }),
+});
